ca6bd9b3b95718e7d0a7da07b09d2d2c5add4d6d
absosm <absosm@gmail.com>
2017-08-05 19:35:32 +0100

    .

--- SmartStore/src/com/home/DataBase.java
+++ SmartStore/src/com/home/DataBase.java
@@ -10,19 +10,30 @@ import javax.swing.JOptionPane;
 
 public class DataBase {
     
-	private int driver;
-    private String host;
-    private int port;
-    private String name;
-    private String username;
-    private String password;
-    private Connection connection;
-    private Statement statement;
+	/**
+	 * déclaration les attributs class "DATABASE"
+	 */
+	private int driver;							// identificateur de base de donnée choisi exp: 0 = MYSQL
+    private String host;						// hôte ou le serveur de base de donnée exp: "localhost"
+    private int port;							// le port associe de base de donnée choisi exp: MySQL = 3306
+    private String name;						// le nom de la base de donnée
+    private String username;					// Utilisateur de base de donnée exp. "root"
+    private String password;					// Mot de passe de base de donnée
+    private Connection connection;				// la liaison que permet de faire tous les opération de base de donné
+    private Statement statement;				// gérer les rquêtes
+    
+    /**
+     * déclaration constants identification des moteur de bases des données
+     */
     
     public static final int MYSQL			= 0;
     public static final int MSSQL			= 1;
     public static final int POSTGRESQL		= 2;
     public static final int ORACLE			= 3;
+    
+    /**
+     * déclaration des méthode get-set associe des attribut
+     */
     
     public String getHost() {
 		return host;
@@ -63,13 +74,16 @@ public class DataBase {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	/**
+	 * constructeur de class "DATABASE"
+	 */
 	public DataBase() {
     	/**
     	 * default MySQL server
     	 */
-    	this.port = 3306;
-    	this.driver = DataBase.MYSQL;
+    	this.port = 3306;				// port par défault de MySQL server
+    	this.driver = DataBase.MYSQL;	// identifier le moteur
     }
     
     public DataBase(int driver, int port) {
@@ -77,12 +91,19 @@ public class DataBase {
         this.port = port;
     }
     
+    /**
+     * Connexion au base de donnée return true si le réussi false si non
+     * @return
+     */
     public boolean Connect() {
         
         try {
             
             String url = "";
             
+            /**
+             * importer les class jdbc selon l'identification de chque base de donnée
+             */
             switch (driver) {
             case DataBase.MYSQL: {
             	Class.forName("com.mysql.jdbc.Driver");
@@ -91,13 +112,14 @@ public class DataBase {
             }
             }
             
+            /**
+             * affecter les renseignement de connexion dedans un class "PROPERTIES"
+             */
             Properties prop = new Properties();
-            
             prop.put("user", this.username);
             prop.put("password", this.password);
             prop.put("useUnicode", "yes");
             prop.put("characterEncoding", "UTF-8");
-            
             connection = DriverManager.getConnection(url, prop);
             
             if (connection != null) {
@@ -112,6 +134,11 @@ public class DataBase {
         }
     }
     
+    /**
+     * retourn le resultat selon le requête qui vous avez saisie dans paramètre sql
+     * @param sql
+     * @return
+     */
     public ResultSet getResult(String sql) {
         try {
             return statement.executeQuery(sql);
@@ -121,10 +148,17 @@ public class DataBase {
         }
     }
 
+    /**
+     * retourn la laiason
+     * @return
+     */
 	public Connection getConnection() {
         return this.connection;
     }
-    
+	
+	/**
+	 * déconnexion la laiason    
+	 */
     public void Disconnect() {
         try {
             connection.close();
