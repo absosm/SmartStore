eebb7d87c62236ddf2024a61300d2b6cebe4e674
absosm <absosm@sony-vaio>
2017-08-14 20:12:49 +0100

    commit

--- SmartStore/src/com/home/Configuration.java
+++ SmartStore/src/com/home/Configuration.java
@@ -2,12 +2,10 @@ package com.home;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Base64;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
-import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
